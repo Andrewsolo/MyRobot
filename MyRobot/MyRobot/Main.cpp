6da81d6ca6e9bb551c93cafda1efd627e405942e
Andrewsolo <andrewsolovyov@gmail.com>
2017-04-27 15:37:50 +0300

    ADXL345 test

--- MyRobot/MyRobot/Main.cpp
+++ MyRobot/MyRobot/Main.cpp
@@ -7,8 +7,13 @@
 #include "MotionAutomatTask.h"
 #include "Motors.h"
 
-//====== Глобальные переменные
 
+#include <Wire.h>
+#include <Adafruit_ADXL345_U.h>
+
+//====== Глобальные переменные
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 //=====================================================================================================================================
 void setup()
@@ -31,6 +36,21 @@ void setup()
 	Serial_Timer	= millis();
 	Servo_Timer		= millis();
 	Sonar_Timer		= millis();	
+
+#ifndef SIMULATOR
+	if(!accel.begin())
+	{
+		/* There was a problem detecting the ADXL345 ... check your connections */
+		Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+		while(1);
+	}
+	accel.setRange(ADXL345_RANGE_16_G);
+	
+	Serial.println("X = " + String(accel.getX()));
+	Serial.println("Y = " + String(accel.getY()));
+	Serial.println("Z = " + String(accel.getZ()));
+#endif	
+
 }
 
 void loop()
